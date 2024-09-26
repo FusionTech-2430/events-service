6d9d91e6f381be5fdb8f0e139691f51bdab060b6
Carlos Santiago Rojas Calderón <carlosantirc@gmail.com>
2024-09-26 03:11:10 -0500

    ✨ Transactional notation

--- src/main/java/co/allconnected/fussiontech/eventsservice/services/FirebaseService.java
+++ src/main/java/co/allconnected/fussiontech/eventsservice/services/FirebaseService.java
@@ -6,6 +6,7 @@ import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.UserRecord;
 import com.google.firebase.cloud.StorageClient;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -14,6 +15,7 @@ import java.io.InputStream;
 @Service
 public class FirebaseService {
 
+    @Transactional
     public String uploadImg(String imageName, String extension, MultipartFile imageFile) throws IOException {
         InputStream inputStream = imageFile.getInputStream();
         Bucket bucket = StorageClient.getInstance().bucket();
@@ -22,11 +24,13 @@ public class FirebaseService {
                 .signUrl(360, java.util.concurrent.TimeUnit.DAYS).toString();
     }
 
+    @Transactional
     public void deleteImg(String imageName) {
         Bucket bucket = StorageClient.getInstance().bucket();
         bucket.get("event_photos/"+imageName).delete();
     }
 
+    @Transactional
     public String createUser(String email, String password) throws FirebaseAuthException {
         UserRecord.CreateRequest request = new UserRecord.CreateRequest()
                 .setEmail(email)
@@ -36,16 +40,19 @@ public class FirebaseService {
         return userRecord.getUid();
     }
 
+    @Transactional
     public void deleteUser(String uid) throws FirebaseAuthException {
         FirebaseAuth.getInstance().deleteUser(uid);
     }
 
+    @Transactional
     public void disableUser(String uid) throws FirebaseAuthException {
         UserRecord.UpdateRequest request = new UserRecord.UpdateRequest(uid)
                 .setDisabled(true);
         FirebaseAuth.getInstance().updateUser(request);
     }
 
+    @Transactional
     public void updateUser(String uid, String email, String password) throws FirebaseAuthException {
         UserRecord.UpdateRequest request = new UserRecord.UpdateRequest(uid);
         if (email != null)
